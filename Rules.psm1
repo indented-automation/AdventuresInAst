eb6372503bda0cfc101e3cf6dd9ac384e3b33ee8
indented-automation <chris@indented.co.uk>
2018-01-25 13:53:26 +0000

    Draft complete

---
+++ Rules.psm1
@@ -0,0 +1,18 @@
+function NoMoreTicks {
+    <#
+    .DESCRIPTION
+        Do not use tick to continue lines.
+    #>
+
+    param (
+        [System.Management.Automation.Language.Token[]]$token
+    )
+
+    $token | Where-Object Kind -eq 'LineContinuation' | ForEach-Object {
+        [Microsoft.Windows.Powershell.ScriptAnalyzer.Generic.DiagnosticRecord]@{
+            RuleName             = $myinvocation.InvocationName
+            Message              = 'Do not use tick for line continuation'
+            Extent               = $_.Extent
+        }
+    }
+}
